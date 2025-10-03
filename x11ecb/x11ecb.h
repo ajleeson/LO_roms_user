f42b4dba0349084aad9d386daca7bcfceb44dcc5
ajleeson <aurorajleeson@gmail.com>
2025-10-03 11:42:14 -0700

    remove k val

--- x11ecb/x11ecb.h
+++ x11ecb/x11ecb.h
@@ -59,7 +59,7 @@
 #  undef INCREASE_NO3LOSS /*jx*/
 #  define BURY           /*jx*/
 /* from Kyle Hinson */
-# define KVALUE_ECB
+# undef KVALUE_ECB
 # define LOSTCO3
 # define OAE_BGC
 /* End from Kyle Hinson */
